190d39657b406063129f431a1b667c8f626f0ec9
Sofia <a.sofia00@hotmail.com>
2019-05-20 00:13:13 +0100

    more things done

--- Cookilicious/LI4/Controllers/MainController.cs
+++ Cookilicious/LI4/Controllers/MainController.cs
@@ -18,5 +18,10 @@ namespace LI4.Controllers
         {
             return View();
         }
+
+        public ActionResult MultiRecipes()
+        {
+            return View();
+        }
     }
 }
